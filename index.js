de7f0ac48fe9eb3ab66563e3ef92f23ec3fbebec
sumeet <sumeet1396@gmail.com>
2023-08-20 01:11:39 +0530

    ping api created

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require('express');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const morgan = require('morgan');
 
 process.on('uncaughtException', (err) => {
     console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
@@ -15,11 +17,17 @@ dotenv.config({ path: './.env' });
   
 const DB = process.env.DB_URL.replace('<PASSWORD>', process.env.MONGO_PASSWORD);
 
+const AppError = require('./utils/appError');
+
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
   })
   .then(() => console.log('DB Connection Successful'));
+
+const app = express();
+const port = process.env.PORT || 5000;
+
 
 // 1) GLOBAL MIDDLEWARES
 // Set security HTTP headers
@@ -33,17 +41,36 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-const app = express();
-const port = process.env.PORT || 5000;
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
 
-  const server = app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+app.use(morgan('dev'));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+
+// ping api
+
+app.get("/ping", (req, res) => {
+    res.send('PING')
+})
+//404 url error handler for all http request methods
+app.all('*', (req, res, next) => {
+    next(new AppError(`Can't found ${req.originalUrl} on the server`, 404));
   });
-  
-  process.on('unhandledRejection', (err) => {
-    console.log('UNHANDLED REJECTION! 💥 Shutting down...');
-    console.log(err.name, err.message);
-    server.close(() => {
-      process.exit(1);
-    });
-  });
+
+const server = app.listen(port, () => {
+console.log(`Example app listening at http://localhost:${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+console.log(err.name, err.message);
+server.close(() => {
+    process.exit(1);
+});
+});
---
+++ utils/appError.js
@@ -0,0 +1,13 @@
+class AppError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+    this.isOperational = true;
+
+    Error.captureStackTrace(this, this.constructor); //stack trace lets us know where the error happens in our code
+  }
+}
+
+module.exports = AppError;
